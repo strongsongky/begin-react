aeead3ef47890a4cfeb525a9bcf11e7a624be4a4
strongsongky <102295416+strongsongky@users.noreply.github.com>
2022-08-09 20:06:26 +0900

    react practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hello from "./Hello";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hello />
+      <Hello />
+      <Hello />
+      <div className="" style={style}>
+        {name}
+      </div>
+    </>
   );
 }
 
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hello() {
+  return (
+    <div>
+      안녕하세요플레<button>요플레</button>
+    </div>
+  );
+}
+
+export default Hello;
